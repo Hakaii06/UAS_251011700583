1babe8406ac43c5028f5cd1f5b94f94e005592ac
Hakaii06 <haikalabdullah356@gmail.com>
2026-06-30 16:52:04 +0700

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,8 +1,15 @@
 <?php
+// 1. Hapus cookie login dengan mengatur waktu kedaluwarsa ke masa lalu
+if (isset($_COOKIE['login_user'])) {
+    setcookie('login_user', '', time() - 3600, '/'); 
+}
+
+// 2. Tetap bersihkan session jika sewaktu-waktu digunakan di halaman lain
 session_start();
 $_SESSION = [];
 session_unset();
 session_destroy();
-header("Location: login");
+
+// 3. Alihkan ke halaman login (gunakan '/' jika routing Vercel Anda menggunakan Clean URLs)
+header("Location: /login");
 exit;
-?>
